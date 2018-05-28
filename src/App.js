502f59d438d907b26fa904574a9062e3a4cd6841
Robert Judson <robjudmello@hotmail.com>
2018-05-28 17:02:49 -0300

    Estruturação dos arquivos

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Link } from 'react-router-dom';
 import Routes from './store/routes';
+
+import Login from './views/containers/screens/login/login';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>HEADER</h1>
-        <Routes />
+      
+      <div>
+        <BrowserRouter>
+          <Routes />
+        </BrowserRouter>
       </div>
     );
   }
--- src/store/routes.jsx
+++ src/store/routes.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Login from '../views/containers/screens/login/login';
+
+const PageNotFound = () => (
+  <h1>
+    Page Not Found
+  </h1>
+)
 
 const Routes = () => (
-    <div></div>
+  <BrowserRouter>
+    <Switch>
+      <Route exact path='/' component={Login} />
+      <Route component={PageNotFound} />
+    </Switch>
+  </BrowserRouter>
 )
 
 export default Routes;
